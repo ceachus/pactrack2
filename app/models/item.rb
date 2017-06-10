d2c70e2a65760889ed4320047b7ff3d77f74ddbd
Claudia Eachus <ceachus@users.noreply.github.com>
2017-06-10 14:17:17 -0700

    updates

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,12 @@
 class Item < ApplicationRecord
+
+
   # belongs_to :package
   # belongs_to :return
   belongs_to :order
   has_one :vendor, :through => :order
   has_one :user, :through => :order, :source => :user
-  belongs_to :package
+  belongs_to :package, optional: true
+
+
 end
